1dd81bc688ad0765820e652b8d6cee4cfd5f1019
Izadora Netz <izadora.ns@gmail.com>
2025-04-07 17:13:59 -0300

    roteiro ate item 6

--- Lab01/dominios.hpp
+++ Lab01/dominios.hpp
@@ -4,7 +4,19 @@
 using namespace std;
 
 class codigoCliente {
+private:
+    static const int LIMITE = 5;
+    static const int DEFAULT = 0;
+    int valor;
+    bool validar(int);
+public:
+    bool setValor(int);
+    int getValor();
 
+};
+
+inline int codigoCliente::getValor(){
+return valor;
 };
 
 #endif // DOMINIOS_HPP_INCLUDED
--- Lab01/main.cpp
+++ Lab01/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include "dominios.hpp"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    CodigoCliente a;
+
+    if(a.setValor(3))
+        cout<<"Valor = "<<a.getValor()<<endl;
+    else
+        cout<<"Valor invalido"<<endl;
+    if(a.setValor(30))
+        cout<<"Valor = "<<a.getValor()<<endl;
+    else
+    cout<<"Valor invalido"<<endl;
     return 0;
+
 }
