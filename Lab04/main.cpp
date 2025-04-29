74b817fefb373c044f5e56098c7a2cc6eb1ca229
Izadora Netz <izadora.ns@gmail.com>
2025-04-29 12:01:09 +0000

    incluindo outras verificacoes

--- Lab04/main.cpp
+++ Lab04/main.cpp
@@ -1,5 +1,3 @@
-// refatorar código para uso de exceções
-
 #include <iostream>
 #include "dominios.hpp"
 
@@ -7,23 +5,37 @@ using namespace std;
 
 int main() {
     cout << "Uso de Exceções: " << endl;
-    
 
     Dominio dominio;
     
+    // Teste com valor válido
     try {
         dominio.setValor("300");
+        cout << "Valor válido definido: " << dominio.getValor() << endl;
     }
-    catch(invalid_argument &exp){
-        cout << "Excecao: " << exp.what() << endl;
+    catch(invalid_argument &exp) {
+        cout << "Excecao (invalid_argument): " << exp.what() << endl;
+    }
+    catch(length_error &exp) {
+        cout << "Excecao (length_error): " << exp.what() << endl;
+    }
+    catch(...) {
+        cout << "Excecao desconhecida" << endl;
     }
 
-    // validor invalido
+    // Teste com valor inválido (muito longo)
     try {
         dominio.setValor("98023984902374983587634875634875638765384765837658736");
+        cout << "Valor válido definido: " << dominio.getValor() << endl;
     }
-    catch(invalid_argument &exp){
-        cout << "Excecao: " << exp.what() << endl;
+    catch(invalid_argument &exp) {
+        cout << "Excecao (invalid_argument): " << exp.what() << endl;
+    }
+    catch(length_error &exp) {
+        cout << "Excecao (length_error): " << exp.what() << endl;
+    }
+    catch(...) {
+        cout << "Excecao desconhecida" << endl;
     }
 
     return 0;
